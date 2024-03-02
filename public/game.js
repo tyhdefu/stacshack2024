4a4d471307dd05fa7f4d2a483d29f0745d5c1eec
James Hendry <jh455@st-andrews.ac.uk>
2024-03-02 14:16:23 +0000

    Add moves and make Monster class

--- public/game.js
+++ public/game.js
@@ -1,37 +1,38 @@
 "use strict"
 
 class Monster {
-    constructor(id, name, types, max_hp, moves) {
-        this.id = id
-        this.name = name
-        this.types = types
-        this.max_hp = max_hp
-        this.hp = max_hp
-        this.moves = moves
+    constructor(id, name, types, sprite_path, max_hp, moves) {
+        this.id = id;
+        this.name = name;
+        this.sprite_path = sprite_path;
+        this.types = types;
+        this.max_hp = max_hp;
+        this.hp = max_hp;
+        this.moves = moves;
     }
 }
 
 class Move {
     constructor(id, types) {
-        this.id = id
-        this.types = types
+        this.id = id;
+        this.types = types;
     }
 }
 
 async function loadMonsterData() {
-    const response = await fetch("/monsters")
-    return response.json()
+    const response = await fetch("/monsters");
+    return response.json();
 }
 
 async function loadMoveData() {
-    const moves = await fetch("./moves")
-    return moves.json()
+    const moves = await fetch("./moves");
+    return moves.json();
 }
 
 function createMonster(id, all_monsters, all_moves) {
     const monster = all_monsters.pokemon[id];
-    const monster_moves = monster[2].map(m => createMove(m, all_moves))
-    return new Monster(monster[0], monster[1], monster_moves, 30, monster[3])
+    const monster_moves = monster[4].map(m => createMove(m.toLowerCase(), all_moves));
+    return new Monster(monster[0], monster[1], monster[2], monster[3], 30, monster_moves);
 }
 
 function createMove(id, moves) {
@@ -42,7 +43,10 @@ function createMove(id, moves) {
     }
 }
 
-loadMonsterData().then((res) => {
-    console.log(res)
+loadMonsterData().then((monsterData) => {
+    console.log(monsterData);
+    loadMoveData().then((moveData) => {
+        console.log(moveData);
+        console.log(createMonster(1, monsterData, moveData));
+    })
 })
-loadMoveData().then((res) => console.log(res))
